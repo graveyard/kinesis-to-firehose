256a9fa0814955d74a0d294ae69d90982632a07a
Nathan Leiby <nathan.leiby@clever.com>
2017-03-28 14:46:07 -0700

    refactor: move things out of main
    combines RecordProcessor and FirehoseWriter

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"context"
-	"encoding/base64"
-	"fmt"
 	"log"
-	"math/big"
 	"os"
 	"strconv"
 	"time"
@@ -18,140 +14,6 @@ import (
 
 	"github.com/Clever/kinesis-to-firehose/writer"
 )
-
-type RecordProcessor struct {
-	shardID           string
-	sleepDuration     time.Duration
-	checkpointRetries int
-	checkpointFreq    time.Duration
-	largestSeq        *big.Int
-	largestSubSeq     int
-	lastCheckpoint    time.Time
-	firehoseWriter    *writer.FirehoseWriter
-	rateLimiter       *rate.Limiter // Limits the number of records processed per second
-}
-
-func NewRecordProcessor(writer *writer.FirehoseWriter, limiter *rate.Limiter) *RecordProcessor {
-	return &RecordProcessor{
-		sleepDuration:     5 * time.Second,
-		checkpointRetries: 5,
-		checkpointFreq:    60 * time.Second,
-		firehoseWriter:    writer,
-		rateLimiter:       limiter,
-	}
-}
-
-func (rp *RecordProcessor) Initialize(shardID string) error {
-	rp.shardID = shardID
-	rp.lastCheckpoint = time.Now()
-	return nil
-}
-
-func (rp *RecordProcessor) checkpoint(checkpointer kcl.Checkpointer, sequenceNumber string, subSequenceNumber int) {
-	for n := -1; n < rp.checkpointRetries; n++ {
-		err := checkpointer.Checkpoint(sequenceNumber, subSequenceNumber)
-		if err == nil {
-			return
-		}
-
-		if cperr, ok := err.(kcl.CheckpointError); ok {
-			switch cperr.Error() {
-			case "ShutdownException":
-				fmt.Fprintf(os.Stderr, "Encountered shutdown exception, skipping checkpoint\n")
-				return
-			case "ThrottlingException":
-				fmt.Fprintf(os.Stderr, "Was throttled while checkpointing, will attempt again in %s", rp.sleepDuration)
-			case "InvalidStateException":
-				fmt.Fprintf(os.Stderr, "MultiLangDaemon reported an invalid state while checkpointing\n")
-			default:
-				fmt.Fprintf(os.Stderr, "Encountered an error while checkpointing: %s", err)
-			}
-		}
-
-		if n == rp.checkpointRetries {
-			fmt.Fprintf(os.Stderr, "Failed to checkpoint after %d attempts, giving up.\n", rp.checkpointRetries)
-			return
-		}
-
-		time.Sleep(rp.sleepDuration)
-	}
-}
-
-// shouldUpdateSequence determines whether a new larger sequence number is available
-func (rp *RecordProcessor) shouldUpdateSequence(sequenceNumber *big.Int, subSequenceNumber int) bool {
-	return rp.largestSeq == nil || sequenceNumber.Cmp(rp.largestSeq) == 1 ||
-		(sequenceNumber.Cmp(rp.largestSeq) == 0 && subSequenceNumber > rp.largestSubSeq)
-}
-
-func (rp *RecordProcessor) ProcessRecords(records []kcl.Record, checkpointer kcl.Checkpointer) error {
-	for _, record := range records {
-		// Wait until rate limiter permits one record to be processed
-		rp.rateLimiter.Wait(context.Background())
-
-		// Base64 decode the record
-		data, err := base64.StdEncoding.DecodeString(record.Data)
-		if err != nil {
-			return err
-		}
-		msg := string(data)
-
-		// Write the message to firehose
-		err = rp.firehoseWriter.ProcessMessage(msg)
-		if err != nil {
-			return err
-		}
-
-		// Handle checkpointing
-		// TODO: How to handle the difference between ProcessMessage (sent to FirehoseOutput) vs successfully sent?
-		seqNumber := new(big.Int)
-		if _, ok := seqNumber.SetString(record.SequenceNumber, 10); !ok {
-			fmt.Fprintf(os.Stderr, "could not parse sequence number '%s'\n", record.SequenceNumber)
-			continue
-		}
-		if rp.shouldUpdateSequence(seqNumber, record.SubSequenceNumber) {
-			rp.largestSeq = seqNumber
-			rp.largestSubSeq = record.SubSequenceNumber
-		}
-	}
-	if time.Now().Sub(rp.lastCheckpoint) > rp.checkpointFreq {
-		rp.checkpoint(checkpointer, rp.largestSeq.String(), rp.largestSubSeq)
-		rp.lastCheckpoint = time.Now()
-
-		// Write status to file
-		err := appendToFile(logFile, fmt.Sprintf("%s -- %s\n", rp.shardID, rp.firehoseWriter.Status()))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func appendToFile(filename, text string) error {
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if _, err = f.WriteString(text); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (rp *RecordProcessor) Shutdown(checkpointer kcl.Checkpointer, reason string) error {
-	if reason == "TERMINATE" {
-		fmt.Fprintf(os.Stderr, "Was told to terminate, will attempt to checkpoint.\n")
-		rp.firehoseWriter.FlushAll()
-		rp.checkpoint(checkpointer, "", 0)
-	} else {
-		fmt.Fprintf(os.Stderr, "Shutting down due to failover. Reason: %s. Will not checkpoint.\n", reason)
-	}
-	return nil
-}
-
-var logFile = "/tmp/kcl_stderr"
 
 func main() {
 	logFile := getEnv("LOG_FILE")
@@ -169,10 +31,7 @@ func main() {
 		FlushInterval:  10 * time.Second,
 		FlushCount:     500,
 		FlushSize:      4 * 1024 * 1024, // 4Mb
-	}
-	writer, err := writer.NewFirehoseWriter(config)
-	if err != nil {
-		log.Fatalf("Failed to create FirehoseWriter: %s", err.Error())
+		LogFile:        logFile,
 	}
 
 	// rateLimit is expressed in records-per-second
@@ -184,8 +43,11 @@ func main() {
 	rateLimit := rate.Limit(rl)
 	burstLimit := int(rl * 1.2)
 
-	rp := NewRecordProcessor(writer, rate.NewLimiter(rateLimit, burstLimit))
-	kclProcess := kcl.New(os.Stdin, os.Stdout, os.Stderr, rp)
+	writer, err := writer.NewFirehoseWriter(config, rate.NewLimiter(rateLimit, burstLimit))
+	if err != nil {
+		log.Fatalf("Failed to create FirehoseWriter: %s", err.Error())
+	}
+	kclProcess := kcl.New(os.Stdin, os.Stdout, os.Stderr, writer)
 	kclProcess.Run()
 }
 
--- writer/firehose_writer.go
+++ writer/firehose_writer.go
@@ -1,23 +1,43 @@
 package writer
 
 import (
+	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"math/big"
 	"os"
 	"sync/atomic"
 	"time"
 
+	"github.com/Clever/amazon-kinesis-client-go/kcl"
 	"github.com/Clever/heka-clever-plugins/batcher"
 	"github.com/aws/aws-sdk-go/service/firehose"
 	iface "github.com/aws/aws-sdk-go/service/firehose/firehoseiface"
+	"golang.org/x/time/rate"
 )
 
-// FirehoseWriter writes record batches to a firehose stream
 type FirehoseWriter struct {
+	shardID string
+	logFile string
+
+	// KCL checkpointing
+	sleepDuration     time.Duration
+	checkpointRetries int
+	checkpointFreq    time.Duration
+	largestSeq        *big.Int
+	largestSubSeq     int
+	lastCheckpoint    time.Time
+
+	// Limits the number of records processed per second
+	rateLimiter *rate.Limiter
+
+	// Firehose Config
 	streamName     string
 	messageBatcher batcher.Batcher
 	firehoseClient iface.FirehoseAPI
 
+	// Firehose metrics
 	recvRecordCount   int64
 	sentRecordCount   int64
 	failedRecordCount int64
@@ -35,10 +55,11 @@ type FirehoseWriterConfig struct {
 	FlushCount int
 	// FlushSize is the size of a batch in bytes that triggers a push to firehose. Max batch size is 4Mb (4*1024*1024), see: http://docs.aws.amazon.com/firehose/latest/dev/limits.html
 	FlushSize int
+	// LogFile is the path to a log file (we write logs in a file since stdout/stderr are used by KCL)
+	LogFile string
 }
 
-// NewFirehoseWriter constructs a FirehoseWriter
-func NewFirehoseWriter(config FirehoseWriterConfig) (*FirehoseWriter, error) {
+func NewFirehoseWriter(config FirehoseWriterConfig, limiter *rate.Limiter) (*FirehoseWriter, error) {
 	if config.FlushCount > 500 || config.FlushCount < 1 {
 		return nil, fmt.Errorf("FlushCount must be between 1 and 500 messages")
 	}
@@ -47,8 +68,13 @@ func NewFirehoseWriter(config FirehoseWriterConfig) (*FirehoseWriter, error) {
 	}
 
 	f := &FirehoseWriter{
-		streamName:     config.StreamName,
-		firehoseClient: config.FirehoseClient,
+		streamName:        config.StreamName,
+		firehoseClient:    config.FirehoseClient,
+		sleepDuration:     5 * time.Second,
+		checkpointRetries: 5,
+		checkpointFreq:    60 * time.Second,
+		rateLimiter:       limiter,
+		logFile:           config.LogFile,
 	}
 
 	f.messageBatcher = batcher.New(f)
@@ -59,8 +85,118 @@ func NewFirehoseWriter(config FirehoseWriterConfig) (*FirehoseWriter, error) {
 	return f, nil
 }
 
+func (f *FirehoseWriter) Initialize(shardID string) error {
+	f.shardID = shardID
+	f.lastCheckpoint = time.Now()
+	return nil
+}
+
+// TODO: refactor KCL checkpointer to include retries / sleep duration
+func (f *FirehoseWriter) checkpoint(checkpointer kcl.Checkpointer, sequenceNumber string, subSequenceNumber int) {
+	for n := -1; n < f.checkpointRetries; n++ {
+		err := checkpointer.Checkpoint(sequenceNumber, subSequenceNumber)
+		if err == nil {
+			return
+		}
+
+		if cperr, ok := err.(kcl.CheckpointError); ok {
+			switch cperr.Error() {
+			case "ShutdownException":
+				fmt.Fprintf(os.Stderr, "Encountered shutdown exception, skipping checkpoint\n")
+				return
+			case "ThrottlingException":
+				fmt.Fprintf(os.Stderr, "Was throttled while checkpointing, will attempt again in %s", f.sleepDuration)
+			case "InvalidStateException":
+				fmt.Fprintf(os.Stderr, "MultiLangDaemon reported an invalid state while checkpointing\n")
+			default:
+				fmt.Fprintf(os.Stderr, "Encountered an error while checkpointing: %s", err)
+			}
+		}
+
+		if n == f.checkpointRetries {
+			fmt.Fprintf(os.Stderr, "Failed to checkpoint after %d attempts, giving up.\n", f.checkpointRetries)
+			return
+		}
+
+		time.Sleep(f.sleepDuration)
+	}
+}
+
+// shouldUpdateSequence determines whether a new larger sequence number is available
+func (f *FirehoseWriter) shouldUpdateSequence(sequenceNumber *big.Int, subSequenceNumber int) bool {
+	return f.largestSeq == nil || sequenceNumber.Cmp(f.largestSeq) == 1 ||
+		(sequenceNumber.Cmp(f.largestSeq) == 0 && subSequenceNumber > f.largestSubSeq)
+}
+
+func (f *FirehoseWriter) ProcessRecords(records []kcl.Record, checkpointer kcl.Checkpointer) error {
+	for _, record := range records {
+		// Wait until rate limiter permits one record to be processed
+		f.rateLimiter.Wait(context.Background())
+
+		// Base64 decode the record
+		data, err := base64.StdEncoding.DecodeString(record.Data)
+		if err != nil {
+			return err
+		}
+		msg := string(data)
+
+		// Write the message to firehose
+		err = f.processMessage(msg)
+		if err != nil {
+			return err
+		}
+
+		// Handle checkpointing
+		seqNumber := new(big.Int)
+		if _, ok := seqNumber.SetString(record.SequenceNumber, 10); !ok {
+			fmt.Fprintf(os.Stderr, "could not parse sequence number '%s'\n", record.SequenceNumber)
+			continue
+		}
+		if f.shouldUpdateSequence(seqNumber, record.SubSequenceNumber) {
+			f.largestSeq = seqNumber
+			f.largestSubSeq = record.SubSequenceNumber
+		}
+	}
+	if time.Now().Sub(f.lastCheckpoint) > f.checkpointFreq {
+		f.checkpoint(checkpointer, f.largestSeq.String(), f.largestSubSeq)
+		f.lastCheckpoint = time.Now()
+
+		// Write status to file
+		err := appendToFile(f.logFile, fmt.Sprintf("%s -- %s\n", f.shardID, f.Status()))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func appendToFile(filename, text string) error {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = f.WriteString(text); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (f *FirehoseWriter) Shutdown(checkpointer kcl.Checkpointer, reason string) error {
+	if reason == "TERMINATE" {
+		fmt.Fprintf(os.Stderr, "Was told to terminate, will attempt to checkpoint.\n")
+		f.FlushAll()
+		f.checkpoint(checkpointer, "", 0)
+	} else {
+		fmt.Fprintf(os.Stderr, "Shutting down due to failover. Reason: %s. Will not checkpoint.\n", reason)
+	}
+	return nil
+}
+
 // ProcessMessage reads an input string and adds it to the batcher, which will ultimately send it
-func (f *FirehoseWriter) ProcessMessage(msg string) error {
+func (f *FirehoseWriter) processMessage(msg string) error {
 	atomic.AddInt64(&f.recvRecordCount, 1)
 
 	// TODO: Fully decode the message
