8011895cdb7b3e94000aa38a2c52ba9a715ba126
Xavi Ramirez <xavi.ramirez@clever.com>
2017-05-19 22:15:25 +0000

    MessageBatcher now keeps track of smallestSeq numbe and expose both largest and smallest sequence number through functions

--- batcher/message_batcher.go
+++ batcher/message_batcher.go
@@ -3,6 +3,7 @@ package batcher
 import (
 	"fmt"
 	"math/big"
+	"sync"
 	"time"
 )
 
@@ -19,9 +20,20 @@ type Batcher interface {
 	AddMessage(msg []byte, sequenceNumber string, subSequenceNumber int) error
 	// Flush all messages from the batch
 	Flush()
+	// SmallestSeqPair returns the smallest SequenceNumber and SubSequence number in
+	// the current batch
+	SmallestSequencePair() (*big.Int, int)
+}
+
+type msgPack struct {
+	msg       []byte
+	seqNumBig *big.Int
+	subSeqNum int
 }
 
 type batcher struct {
+	mux sync.Mutex
+
 	flushInterval time.Duration
 	flushCount    int
 	flushSize     int
@@ -31,8 +43,13 @@ type batcher struct {
 	largestSeq    *big.Int
 	largestSubSeq int
 
+	// smallestSeq and smallestSubSeq are used to track the highest sequence number
+	// of any record in the batch. This is used for checkpointing.
+	smallestSeq    *big.Int
+	smallestSubSeq int
+
 	sync      Sync
-	msgChan   chan<- []byte
+	msgChan   chan<- msgPack
 	flushChan chan<- struct{}
 }
 
@@ -42,7 +59,7 @@ type batcher struct {
 // - flushCount - number of messages that trigger a flush (default 10).
 // - flushSize - size of batch that triggers a flush (default 1024 * 1024 = 1 mb)
 func New(sync Sync, flushInterval time.Duration, flushCount int, flushSize int) Batcher {
-	msgChan := make(chan []byte, 100)
+	msgChan := make(chan msgPack, 100)
 	flushChan := make(chan struct{})
 
 	b := &batcher{
@@ -57,6 +74,20 @@ func New(sync Sync, flushInterval time.Duration, flushCount int, flushSize int) 
 	go b.startBatcher(msgChan, flushChan)
 
 	return b
+}
+
+func (b *batcher) SmallestSequencePair() (*big.Int, int) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
+	return b.smallestSeq, b.smallestSubSeq
+}
+
+func (b *batcher) LargestSequencePair() (*big.Int, int) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
+	return b.largestSeq, b.largestSubSeq
 }
 
 func (b *batcher) SetFlushInterval(dur time.Duration) {
@@ -76,30 +107,37 @@ func (b *batcher) AddMessage(msg []byte, sequenceNumber string, subSequenceNumbe
 		return fmt.Errorf("Empty messages can't be sent")
 	}
 
-	err := b.updateSequenceNumber(sequenceNumber, subSequenceNumber)
-	if err != nil {
-		return err
-	}
-
-	b.msgChan <- msg
-	return nil
-}
-
-// updateSequenceNumber is used to track the highest sequenceNumber of any record in the batch.
-// When flush() is called, the batcher sends the sequence number to the writer. When the writer
-// checkpoints, it does so up to the latest message that was flushed successfully.
-func (b *batcher) updateSequenceNumber(sequenceNumber string, subSequenceNumber int) error {
 	seqNumBig := new(big.Int)
-	if _, ok := seqNumBig.SetString(sequenceNumber, 10); !ok {
+	if _, ok := seqNumBig.SetString(sequenceNumber, 10); !ok { // Validating sequenceNumber
 		return fmt.Errorf("could not parse sequence number '%s'", sequenceNumber)
 	}
 
-	// Check if new seqNumBig is larger
-	if b.largestSeq == nil || seqNumBig.Cmp(b.largestSeq) == 1 || (seqNumBig.Cmp(b.largestSeq) == 0 && subSequenceNumber > b.largestSubSeq) {
-		b.largestSeq = seqNumBig
-		b.largestSubSeq = subSequenceNumber
-	}
+	b.msgChan <- msgPack{msg, seqNumBig, subSequenceNumber}
 	return nil
+}
+
+// updateSequenceNumbers is used to track the highest sequenceNumber of any record in the batch.
+// When flush() is called, the batcher sends the sequence number to the writer. When the writer
+// checkpoints, it does so up to the latest message that was flushed successfully.
+func (b *batcher) updateSequenceNumbers(seqNumBig *big.Int, subSeqNum int) {
+	b.mux.Lock()
+	defer b.mux.Unlock()
+
+	isLarger := b.largestSeq == nil ||
+		seqNumBig.Cmp(b.largestSeq) == 1 ||
+		(seqNumBig.Cmp(b.largestSeq) == 0 && subSeqNum > b.largestSubSeq)
+	if isLarger {
+		b.largestSeq = seqNumBig
+		b.largestSubSeq = subSeqNum
+	}
+
+	isSmaller := b.smallestSeq == nil ||
+		seqNumBig.Cmp(b.smallestSeq) == -1 ||
+		(seqNumBig.Cmp(b.smallestSeq) == 0 && subSeqNum < b.smallestSubSeq)
+	if isSmaller {
+		b.smallestSeq = seqNumBig
+		b.smallestSubSeq = subSeqNum
+	}
 }
 
 func (b *batcher) Flush() {
@@ -118,11 +156,12 @@ func (b *batcher) batchSize(batch [][]byte) int {
 func (b *batcher) flush(batch [][]byte) [][]byte {
 	if len(batch) > 0 {
 		b.sync.SendBatch(batch, b.largestSeq, b.largestSubSeq)
+		b.smallestSeq = nil
 	}
 	return [][]byte{}
 }
 
-func (b *batcher) startBatcher(msgChan <-chan []byte, flushChan <-chan struct{}) {
+func (b *batcher) startBatcher(msgChan <-chan msgPack, flushChan <-chan struct{}) {
 	batch := [][]byte{}
 
 	for {
@@ -131,13 +170,14 @@ func (b *batcher) startBatcher(msgChan <-chan []byte, flushChan <-chan struct{})
 			batch = b.flush(batch)
 		case <-flushChan:
 			batch = b.flush(batch)
-		case msg := <-msgChan:
+		case pack := <-msgChan:
 			size := b.batchSize(batch)
-			if b.flushSize < size+len(msg) {
+			if b.flushSize < size+len(pack.msg) {
 				batch = b.flush(batch)
 			}
 
-			batch = append(batch, msg)
+			batch = append(batch, pack.msg)
+			b.updateSequenceNumbers(pack.seqNumBig, pack.subSeqNum)
 
 			if b.flushCount <= len(batch) || b.flushSize <= b.batchSize(batch) {
 				batch = b.flush(batch)
